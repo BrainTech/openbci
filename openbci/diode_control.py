7ee2d8ae42b60763e62f054e92f8e8a925ec3f1c
Magdalena Michalska <magdalena.alicja.michalska@gmail.com>
2009-11-02 21:04:53 +0000

    preambule

--- openbci/diode_control.py
+++ openbci/diode_control.py
@@ -1,5 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# OpenBCI - framework for Brain-Computer Interfaces based on EEG signal
+# Project was initiated by Magdalena Michalska and Krzysztof Kulewski
+# as part of their MSc theses at the University of Warsaw.
+# Copyright (C) 2008-2009 Krzysztof Kulewski and Magdalena Michalska
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author:
+#      Krzysztof Kulewski <kulewski@gmail.com>
+#      Magdalena Michalska <jezzy.nietoperz@gmail.com>
+#
+
+
+
 import numpy, cPickle, os, time, sys, random, samples_pb2, variables_pb2, blinker 
 from multiplexer.multiplexer_constants import peers, types
 from multiplexer.clients import BaseMultiplexerServer, connect_client
