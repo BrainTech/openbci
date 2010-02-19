41b4f2878f6d7c6c1b792769eb8866a206c9fb5e
Łukasz Polak <l.polak@gmail.com>
2010-02-19 18:11:37 +0000

    Poprawki do skryptu UGMa, odświeżanego z pliku co sekundę

--- openbci/ugm/run_refreshing_ugm.py
+++ openbci/ugm/run_refreshing_ugm.py
@@ -7,10 +7,14 @@ class UgmTestServer(object):
         print("Ugm has just been displayed. It refreshes from ugm.configs.ugm_config file every 1 second, so feel free to modify the file and inspect results.")
         l_config_manager = ugm_config_manager.UgmConfigManager('ugm.configs.ugm_config')
         l_engine = ugm_engine.UgmEngine(l_config_manager)
-        l_engine_thread = thread.start_new_thread(l_engine.run, ())
+        l_thread = thread.start_new_thread(self.update_engine, (l_config_manager, l_engine))
+        l_engine.run()
+            
+    def update_engine(self, p_config_manager, p_engine):
+        time.sleep(5)
         while True:
             time.sleep(1)
-            l_config_manager.update_from_file()
-            l_engine.update()
+            p_config_manager.update_from_file()
+            p_engine.rebuild()
 if __name__ == '__main__':
     UgmTestServer()
