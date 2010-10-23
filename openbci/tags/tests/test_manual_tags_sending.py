d928669b94dddc3ced142aaf3994baf1dc3ce01b
Mateusz Kruszyński <mateusz.kruszynski@titanis.pl>
2010-10-23 17:22:05 +0000

    better tags generation

--- openbci/tags/tests/test_manual_tags_sending.py
+++ openbci/tags/tests/test_manual_tags_sending.py
@@ -5,10 +5,14 @@ TAGGER = tagger.get_tagger()
 import random
 def run():
     while True:
-        raw_input()
+        i = raw_input()
+        print("LEN i: "+str(len(i)))
+        if len(i) == 0:
+            i = "XYZ"
+        i =  i.strip()
         t= time.time()
-        print("SEND TAG name 'XYZ' with time: "+repr(t))
-        TAGGER.send_tag(t, t+3.0*random.random()+1.0, 'XYZ', {'a':1, 'b':2.2, 'c':'napis_jakis'})
+        print("SEND TAG name"+i+" with time: "+repr(t))
+        TAGGER.send_tag(t, t+3.0*random.random()+1.0, i, {'a':1, 'b':2.2, 'c':'napis_jakis', 'annotation':"ANNOTACJA JAKAS"})
 
 if __name__ == "__main__":
     print("Type anything to send tag!!!")
