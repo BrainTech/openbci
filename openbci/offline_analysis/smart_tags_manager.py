d495d2469291d3c2ce39a5958d9c7f2e7e6db6a8
Mateusz Kruszyński <mateusz.kruszynski@titanis.pl>
2010-11-18 12:53:09 +0000

    Fix smart tags, so that this module can cope with files stored in a new svarog-compatible format

--- openbci/offline_analysis/smart_tags_manager.py
+++ openbci/offline_analysis/smart_tags_manager.py
@@ -32,7 +32,7 @@ import Queue
 LOGGER = logger.get_logger("smart_tags_manager")
 
        
-class SmartTagsManager(object):
+class ObciSmartTagsManager(object):
     """By now manager gets in init tag definition object and dictionary of
     files paths. Regarding tag definition it iterates smart tags from file
     in iter_smart_tags().
@@ -157,7 +157,7 @@ class SmartTagsManager(object):
         and you will get samples for all channels for that smart tag in format:
         [[list of all samples from ch 0], [list of all samples from ch 1] ...]
         """
-        LOGGER.debug("FIRST SAMPLE TIMESTMP: "+str(self._first_sample_timestamp))
+        #LOGGER.debug("FIRST SAMPLE TIMESTMP: "+str(self._first_sample_timestamp))
         for i_st in self._smart_tags:
             try:
                 # First needed sample timestamp
@@ -215,4 +215,176 @@ class SmartTagsManager(object):
 
     def get_sampling_rate(self):
         return self._sampling_rate
+
+
+
+class SmartTagsManager(object):
+    """By now manager gets in init tag definition object and dictionary of
+    files paths. Regarding tag definition it iterates smart tags from file
+    in iter_smart_tags().
+    Public interface:
+    - __init__()
+    - iter_smart_tags()
+    """
+    def __init__(self, p_tag_def, p_files):
+        """Init all needed slots, read tags file, init smart tags.
+        Parameters:
+        - p_tag_def - an instance of tag definition object
+        (see smart_tag_definition.py)
+        - p_files - a dictionary with files paths:
+        'info' - info file
+        'data' - data file
+        'tags' - tags file
+        """
+        #Open files
+        self._read_manager = signalml_read_manager.SignalmlReadManager(
+            p_files['info'],
+            p_files['data'],
+            p_files['tags'])
+        self._read_manager.start_reading()
+
+        #Init smart tags, here just create smart tags objects without
+        #data slot set.
+
+        #PUBLIC SLOTS
+        self.num_of_channels = int(self._read_manager.get_param(
+            'number_of_channels'))
+        l_freq_str = self._read_manager.get_param('sampling_frequency')
+        self.sampling_freq = float(l_freq_str)
+        #PUBLIC SLOTS
+        self._sample_duration = 1/float(self.sampling_freq)
+        self._samples_no = 0
+
+        self._smart_tags = []
+        self._init_smart_tags(p_tag_def)
+
+    def __str__(self):
+        return str({'tags_num': len(self._smart_tags),
+                'num_of_channels': self.num_of_channels,
+                'sampling_freq': self.sampling_freq})
+        
+    def get_signal_param(self, p_param):
+        return self._read_manager.get_param(p_param)
+
+    def get_smart_tags_number(self):
+        return len(self._smart_tags)
+
+    def _init_smart_tags(self, p_tag_def):
+        """Init smart tags depending on given smart tag definition."""
+        LOGGER.info("Start initialising smart tags.")
+        if p_tag_def.is_type("duration"):
+            self._init_duration_smart_tags(p_tag_def)
+            LOGGER.info("Finished initialising smart tags.")
+        elif p_tag_def.is_type("end_tag"):
+            self._init_end_tag_smart_tags(p_tag_def)
+            LOGGER.info("Finished initialising smart tags.")
+        else:
+            LOGGER.error("Unrecognised tag definition type.")
+            raise(Exception("Unrecognised tag definition type."))
+
+    def _init_end_tag_smart_tags(self, p_tag_def):
+        """Read all tags from file - create smart tags objects while doing it.
+        End-tag-smart-tag is tag that begins and end with some particular tag.
+        """
+
+        l_st_queue = Queue.Queue() # Tags waiting for end tag
+        while True:
+            try: # Iterate over tags from file 
+                i_tag = self._read_manager.get_next_tag()
+            except data_storage_exceptions.NoNextTag:
+                break # All tags has been red
+            if i_tag['name'] in p_tag_def.end_tags_names:
+                # Ending tag is met - set this tag to all smart tags waiting
+                # in the queue
+                while True:
+                    try:
+                        i_st = l_st_queue.get_nowait()
+                    except:
+                        break
+                    i_st.set_end_tag(i_tag)
+                    self._smart_tags.append(i_st)
+            if i_tag['name'] == p_tag_def.start_tag_name:
+                # Create new smart tag, append it to queue
+                st = smart_tag.SmartTagEndTag(p_tag_def, i_tag)
+                if 'self' in p_tag_def.end_tags_names:
+                    # end_tags_names contains 'self' literal which means
+                    # that st`s end tag is its start tag
+                    st.set_end_tag(i_tag)
+                    self._smart_tags.append(st)
+                else:
+                    l_st_queue.put(st)
+
+    def _init_duration_smart_tags(self, p_tag_def):
+        """Read all tags from file - create smart tags objects while doing it.
+        Duration-smart-tag is tag that begins with some particular tag.
+        """
+        while True:
+            try:
+                i_tag = self._read_manager.get_next_tag()
+            except data_storage_exceptions.NoNextTag:
+                break
+            if i_tag['name'] == p_tag_def.start_tag_name:
+                st = smart_tag.SmartTagDuration(p_tag_def, i_tag)
+                self._smart_tags.append(st)
+
+    def iter_smart_tags(self):
+        """This is an iterator, so use id like:
+        for i in mgr.iter_smart_tags():
+            ....
+        It iterates SmartTag objects. You can call get_data() on smart tag,
+        and you will get samples for all channels for that smart tag in format:
+        [[list of all samples from ch 0], [list of all samples from ch 1] ...]
+        """
+        #LOGGER.debug("FIRST SAMPLE TIMESTMP: "+str(self._first_sample_timestamp))
+        for i_st in self._smart_tags:
+            try:
+
+                if not i_st.data_empty():
+                    yield i_st
+                    continue
+
+                # First needed sample timestamp
+                l_start_ts = i_st.get_start_timestamp() 
+                l_samples_to_start =  int(l_start_ts * self.sampling_freq)
+
+                # Last needed sample timestamp
+                l_end_ts = i_st.get_end_timestamp() 
+                l_samples_to_end = int(l_end_ts * self.sampling_freq)
+
+                LOGGER.debug("Tag start timestamp: "+str(l_start_ts))
+                LOGGER.debug("To start tag samples:: "+str(l_samples_to_start))
+                LOGGER.debug("Tag end timestamp: "+str(l_end_ts))
+                LOGGER.debug("Tag end tag samples: "+str(l_samples_to_end))
+
+
+                # To-be-returned data
+                l_data = [[] for i in range(self.num_of_channels)] 
+
+                # Set read manager pointer to start sample 
+                self._read_manager.goto_value(
+                    self.num_of_channels*l_samples_to_start)
+                l_samples_no = l_samples_to_start
+                
+                LOGGER.debug("SAMPLES NO START: "+str(l_samples_no))
+                # Read samples until find first interesting sample
+                while l_samples_no < l_samples_to_end:
+                    for i in range(self.num_of_channels):
+                        l_data[i].append(self._read_manager.get_next_value())
+                    l_samples_no = l_samples_no + 1
+                LOGGER.debug("SAMPLES NO END: "+str(l_samples_no))
+
+                #Now l_data contains samples from all channels between
+                #l_start and l_end timestamp
+                i_st.set_data(l_data) # Set gathered data to smart tag
+                yield i_st # Return smart tag filled with data
+            except data_storage_exceptions.NoNextValue:
+                LOGGER.info("No samples left. Some smart tags could have been ignored.")
+                break # Exit the loop, end iterator
+        LOGGER.info("Finished smart tags iteration")
+        self._samples_no = 0 
+        #Reset samples count so that next 
+        #iter_smart_tags call will work
+        
+    def get_sampling_rate(self):#todo
+        return self.sampling_freq
 
--- openbci/offline_analysis/tests/test_smart_tags_manager.py
+++ openbci/offline_analysis/tests/test_smart_tags_manager.py
@@ -4,60 +4,50 @@
 
 >>> from openbci.offline_analysis import smart_tag_definition as df    
 
->>> d = df.SmartTagDurationDefinition(start_tag_name='ugm_update', start_offset=0, end_offset=0, duration=5.0)
+>>> d = df.SmartTagDurationDefinition(start_tag_name='trigger', start_offset=0, end_offset=0, duration=1.0)
 
->>> f = {'info': 'openbci/offline_analysis/tests/MATI_FULL_EXP_28_10_2010_2.obci.info', 'data':'openbci/offline_analysis/tests/MATI_FULL_EXP_28_10_2010_2.obci.dat', 'tags':'openbci/offline_analysis/tests/MATI_FULL_EXP_28_10_2010_2.obci.tags'}
+>>> f = {'info': 'openbci/offline_analysis/tests/nic.obci.svarog.info', 'data':'openbci/offline_analysis/tests/nic.obci.dat', 'tags':'openbci/offline_analysis/tests/nic.obci.svarog.tags'}
 
 >>> fabricate_data_file(f['data'])
 
 >>> m = mgr.SmartTagsManager(d, f)
 
 >>> print(len(m._smart_tags))
-337
+51
 
 >>> tags, num = iter_all_tags(m)
 
 >>> print(num)
-337
+51
 
 >>> print(repr(tags[0].get_start_timestamp()))
-1275066798.1484799
+0.36085605621337891
 
->>> print(repr(tags[336].get_end_timestamp()))
-1275067017.938139
-
->>> print(repr(tags[2].get_start_timestamp()))
-1275066802.4346509
+>>> print(repr(tags[50].get_end_timestamp()))
+79.996880054473877
 
 >>> print(tags[2].get_data_for(0)[0])
-372117.0
+19389.0
 
->>> print(tags[2].get_data_for(2)[0])
-372119.0
+>>> print(tags_len_ok(tags))
+True
 
->>> print(len(tags[38].get_data_for(0)))
-1280
-
->>> print(len(tags[66].get_data_for(10)))
-1280
-
->>> print(len(tags[222].get_data_for(12)))
-1280
-
->>> print(len(tags[311].get_data_for(20)))
-1280
-
->>> print(len(tags[300].get_data_for(1)))
-1280
-
->>> print(tags[335].get_data_for(15)[2])
-1602793.0
-
->>> dd = df.SmartTagEndTagDefinition(start_tag_name='ugm_update', start_offset=0, end_offset=0, end_tags_names=['ugm_update'])
+>>> dd = df.SmartTagEndTagDefinition(start_tag_name='trigger', start_offset=0, end_offset=0, end_tags_names=['trigger'])
 
 >>> mm = mgr.SmartTagsManager(dd, f)
 
-"""
+>>> tags, num = iter_all_tags(mm)
+
+>>> print(num)
+50
+
+>>> print(not_duration_tags_data_len(tags))
+20131
+
+>>> print(not_duration_tags_data_sub(tags))
+True
+
+""" 
     
 def iter_first_tag(m):
     for st in m.iter_smart_tags():
@@ -72,6 +62,37 @@ def iter_all_tags(m):
         sts.append(st)
     return sts, count
 
+def tags_len_ok(tags):
+    for tag in tags:
+        for i in range(23):
+            if len(tag.get_data_for(i)) != 256:
+                return False
+    return True
+
+def not_duration_tags_data_len(tags):
+    l = 0
+    for tag in tags:
+        l += len(tag.get_data_for(0))
+    return l
+
+def not_duration_tags_data_sub(tags):
+    """True if data in tags are sequential - 
+    they should be, as we have trigger-to-trigger tags
+    and data in file are sequential."""
+    d = tags[0].get_data_for(0)[0]-1
+    x = []
+    ret = True
+    for tag in tags:
+        td_len = len(tag.get_data_for(0))
+        for j in range(td_len):
+            for i in range(23):
+                td = tag.get_data_for(i)
+                if td[j] != d+1:
+                    print td[j]
+                    ret = False
+                d = td[j]
+    return ret
+    
 def fabricate_data_file(f):
     import struct
     freq, ch_count = (256, 23)
