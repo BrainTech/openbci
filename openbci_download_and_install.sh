b837a1336633d9e6b10abcb709dae760a42e7c6b
Mateusz Kruszyński <mateusz.kruszynski@titanis.pl>
2011-10-21 19:53:16 +0200

    Openbci download_and_install scripts

---
+++ openbci_download_and_install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt-get install git-core g++ python-dev maven2 openjdk-6-jdk patch libboost-all-dev libbluetooth-dev fxload xsel python-dev python-serial python-pygame python-scipy python-numpy python-sip python-qt4 python-bluetooth gnulib
+cd
+wget http://protobuf.googlecode.com/files/protobuf-2.4.1.tar.bz2
+tar -xvf protobuf-2.4.1.tar.bz2
+cd protobuf-2.4.1
+./configure --prefix=/usr/
+make
+sudo make install
+cd python
+echo "export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp; python setup.py install" | sudo sh
+cd ../java
+mvn test && mvn install
+cd
+git clone http://escher.fuw.edu.pl/git/http/azouk-libraries
+git clone http://escher.fuw.edu.pl/git/http/openbci
+cd openbci
+./install_mx_and_drivers.sh
+
+
---
+++ openbci_download_and_install_dev.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+p=$1
+if [ -z $p ]
+then
+	echo "Add ssh user name as a first argument, eg: ./openbci_download_and_install_dev.sh mati"
+	exit -1
+fi
+sudo apt-get install git-core g++ python-dev maven2 openjdk-6-jdk patch libboost-all-dev libbluetooth-dev fxload xsel python-dev python-serial python-pygame python-scipy python-numpy python-sip python-qt4 python-bluetooth gnulib
+cd
+wget http://protobuf.googlecode.com/files/protobuf-2.4.1.tar.bz2
+tar -xvf protobuf-2.4.1.tar.bz2
+cd protobuf-2.4.1
+./configure --prefix=/usr/
+make
+sudo make install
+cd python
+echo "export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp; python setup.py install" | sudo sh
+cd ../java
+mvn test && mvn install
+cd
+git clone ssh://$1@escher.fuw.edu.pl/git/azouk-libraries
+git clone ssh://$1@escher.fuw.edu.pl/git/openbci
+cd openbci
+./install_mx_and_drivers.sh
