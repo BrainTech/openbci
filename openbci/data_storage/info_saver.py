4bbce5edd4aa6c7f22b8016e675ff659cb32de83
Mateusz Kruszyński <mateusz.kruszynski@titanis.pl>
2012-01-10 22:34:33 +0100

    Driver - data storage scenario - part 1

--- openbci/data_storage/info_saver.py
+++ openbci/data_storage/info_saver.py
@@ -1,31 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-# OpenBCI - framework for Brain-Computer Interfaces based on EEG signal
-# Project was initiated by Magdalena Michalska and Krzysztof Kulewski
-# as part of their MSc theses at the University of Warsaw.
-# Copyright (C) 2008-2009 Krzysztof Kulewski and Magdalena Michalska
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
 # Author:
 #     Mateusz Kruszyński <mateusz.kruszynski@gmail.com>
 
-
 from multiplexer.multiplexer_constants import peers, types
 from multiplexer.clients import BaseMultiplexerServer
-
+from obci_control.peer.configured_multiplexer_server import ConfiguredMultiplexerServer
 #import signalml_save_manager
 import sys, time, os.path
 import settings, variables_pb2
@@ -34,24 +14,23 @@ import data_storage_logging as logger
 from openbci.offline_analysis.obci_signal_processing.signal import info_file_proxy
 
 LOGGER = logger.get_logger("info_saver", 'info')
-
-#TIMESTAMPS_AS_SEPARATE_CHANNEL = False
 INFO_FILE_EXTENSION = ".obci.info"
-class InfoSaver(BaseMultiplexerServer):
+
+
+class InfoSaver(ConfiguredMultiplexerServer):
     """A class for creating a manifest file with metadata."""
     def __init__(self, addresses):
         super(InfoSaver, self).__init__(addresses=addresses, 
                                           type=peers.INFO_SAVER)
-
-        l_f_name =  self.conn.query(message = "SaveFileName", 
-                                    type = types.DICT_GET_REQUEST_MESSAGE, 
-                                    timeout = 1).message
-        l_f_dir = self.conn.query(message = "SaveFilePath", 
-                                   type = types.DICT_GET_REQUEST_MESSAGE, 
-                                   timeout = 1).message
+        self.configure()
+        #local params
+        l_f_name =  self.config.get_param("save_file_name")
+        l_f_dir = self.config.get_param("save_file_path")
         l_file_path = os.path.normpath(os.path.join(
                l_f_dir, l_f_name + INFO_FILE_EXTENSION))
         self._info_proxy = info_file_proxy.InfoFileWriteProxy(l_file_path)
+
+        #external params
 
     def handle_message(self, mxmsg):
         """Handle messages:
