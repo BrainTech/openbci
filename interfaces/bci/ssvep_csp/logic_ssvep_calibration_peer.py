31a4e1240fed01aa462c083ce0ed1c66bc485939
Mateusz Kruszyński <mateusz.kruszynski@titanis.pl>
2012-02-05 13:21:39 +0100

    System structure refactoring - ssvep calibration minor fix

--- interfaces/bci/ssvep_csp/logic_ssvep_calibration_peer.py
+++ interfaces/bci/ssvep_csp/logic_ssvep_calibration_peer.py
@@ -111,8 +111,8 @@ class LogicSSVEPCalibration(ConfiguredClient):
         
     def run(self):
         ugm_helper.send_text(self.conn, self.hi_text)
-        #keystroke.wait([" "])
-        time.sleep(5)
+        keystroke.wait([" "])
+        #time.sleep(5)
         LOGGER.info("Send begin config ...")
         ugm_helper.send_config(self.conn, self.ugm)
 
