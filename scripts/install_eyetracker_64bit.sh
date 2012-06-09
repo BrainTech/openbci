eca560b3687ab91bf2ee328b7f082486b39b12fc
Karol Auguštin <karol@augustin.pl>
2012-06-09 21:57:41 +0200

    Small fix and rename eyetracker install script

--- scripts/install_eyetracker_64bit.sh
+++ scripts/install_eyetracker_64bit.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-cd ~
+cd ../../
 wget http://www.openframeworks.cc/versions/preRelease_v0.07/of_preRelease_v007_linux64.tar.gz
 tar -xf of_preRelease_v007_linux64.tar.gz
 mv of_preRelease_v007_linux64 openFrameworks
-cd ~/openFrameworks/scripts/linux/ubuntu
+cd openFrameworks/scripts/linux/ubuntu
 sudo ./install_dependencies.sh
-cd
+cd ../../../../
 sudo apt-get install libgsl0-dev
 git clone git://escher.fuw.edu.pl/git/eyetracker
-cd ~/eyetracker/eyetracker/
+cd eyetracker/eyetracker/
 make
 sudo make install
-
+cd ../../
 
 
 
