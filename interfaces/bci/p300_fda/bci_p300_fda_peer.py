aeb91de1d4fdd94be2954d19ce60ec75ee4042c3
Dawid Laszuk <laszukdawid@gmail.com>
2012-06-25 18:28:04 +0200

    p300_fda: little changes

--- interfaces/bci/p300_fda/bci_p300_fda_peer.py
+++ interfaces/bci/p300_fda/bci_p300_fda_peer.py
@@ -17,7 +17,7 @@ from interfaces import interfaces_logging as logger
 from analysis.buffers import auto_blink_buffer
 from interfaces.bci.p300_fda import bci_p300_fda_analysis
 
-from interfaces.bci.ssvep_csp import ssvep_csp_helper
+from interfaces.bci.p300_fda import csp_helper
 from utils import streaming_debug
 
 LOGGER = logger.get_logger("bci_p300_csp", "info")
