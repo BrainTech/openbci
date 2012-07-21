1d542a0e0fa07d719f7c9e857d53dcd586b918d6
Mateusz Kruszyński <mateusz.kruszynski@titanis.pl>
2012-07-21 20:48:17 +0200

    add generic transform engine

--- logic/engines/transform_engine.py
+++ logic/engines/transform_engine.py
@@ -12,11 +12,8 @@ LOGGER = logger.get_logger("transform_engine", "info")
 
 class TransformEngine(object):
     def __init__(self, configs):
+        self._configs = configs
         self._current_interface = configs['default_mode']
-        self._scens = {
-            'switch': configs['switch'],
-            'ssvep': configs['ssvep']
-            }
         self._is_transforming = False
 
     def transform_scenario(self, to_interface):
@@ -24,9 +21,9 @@ class TransformEngine(object):
             return
         elif not self._is_transforming:#can fire only once...
             self._is_transforming = True
-            if self._current_interface == 'ssvep':
+            if self._current_interface.startswith('ssvep'):
                 diode_helper.diode_stop(self.conn)
 
             self._current_interface = to_interface
-            logic_helper.restart_scenario(self.conn, self._scens[to_interface],
+            logic_helper.restart_scenario(self.conn, self._configs[to_interface],
                                           leave_on=['amplifier'])
