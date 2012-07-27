3395d25bba6c74c790cea92801806d6a16afb8ea
Mateusz Kruszyński <mateusz.kruszynski@titanis.pl>
2012-07-27 10:36:45 +0200

    Multiple munich speller without p300

---
+++ scenarios/app/configs/config_app.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from logic.configs import config_speller_8
+from logic.configs import config_robot_8
+class Config(object):
+    def __init__(self):
+        self.number_of_decisions = 8
+        speller = config_speller_8.Config()
+        robot = config_robot_8.Config()
+
+        self.state = []
+        self.actions = []
+        self.letters = []
+
+        #MENU
+        menu_state = 0
+        self.letters.append([u"Speller",u"Robot"
+                             ,"Media", "", #u"High SSVEP",u"Low SSVEP"
+                             u"", u"", u"", u"Quit"])
+        self.actions.append([
+                "",
+                "start_robot_feedback()", 
+                "", 
+                "", 
+                "", "", "", "transform_scenario('main_menu')"])
+        self.state.append([0]*self.number_of_decisions)
+        self._setup_menu()
+        zero_state = 1
+
+        #SPELLER
+        speller_state = zero_state
+        for i, s in enumerate(speller.state):
+            self.state.append([x+speller_state for x in s])
+            self.actions.append(speller.actions[i])
+            self.letters.append(speller.letters[i])
+        self.state[zero_state][-1] = 0 #GOTO MENU
+        self.actions[zero_state][-1] = "clear()"
+        zero_state += len(speller.state)
+
+        #ROBOT
+        robot_state = zero_state
+        for i, s in enumerate(robot.state):
+            self.state.append([x+robot_state for x in s])
+            self.actions.append(robot.actions[i])
+            self.letters.append(robot.letters[i])
+        self.state[zero_state][-1] = 0 #GOTO MENU
+        self.actions[zero_state][-1] = "stop_robot_feedback()"
+        zero_state += len(robot.state)
+
+
+        self.state[menu_state][0] = speller_state
+        self.state[menu_state][1] = robot_state
+
+
+        self.number_of_states = zero_state
+        self.states_configs = ['state', 'letters', 'actions', 'letters_solver', 'actions_solver']
+        self.other_configs = []
+        self.letters_solver = self.number_of_states * [self.number_of_decisions * [""]]
+        self.actions_solver = self.number_of_states * [self.number_of_decisions * [""]]
+
+
+    def _setup_menu(self):
+        pass
---
+++ scenarios/app/configs/config_main_menu.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Config(object):
+    def __init__(self):
+        self.number_of_decisions = 8
+        self.number_of_states = 1
+        # A list of all configs defined for every single state.
+        self.states_configs = ['state', 'letters', 'actions', 'letters_solver', 'actions_solver']
+        # A list of all configs defined as globals, 
+        # not assigned to any particular state.
+        self.other_configs = []
+
+        # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        # !!! Only keys defined in states_configs and other_configs 
+        # will be visible in your application.!!!
+        # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        
+        # States transition matrix
+        self.state = self.number_of_states * [self.number_of_decisions * [0]]
+
+       # Letters definition for every state. Normally for every state it should be a collection of strings.
+        self.letters = self.number_of_states * [self.number_of_decisions * [""]]
+        self.letters[0] = [u"Switch",u"P300",u"SSVEP",u"",
+                           u"", u"P300 Calib", u"SSVEP Calib", u"Quit"]
+        self.letters_solver = self.number_of_states * [self.number_of_decisions * [""]]
+        
+        # thanks to corresponding values from actions_solver obci will decide which program to use.
+        self.actions = self.number_of_states * [self.number_of_decisions * [""]]
+        self.actions[0] = ["transform_scenario('switch')", 
+                           "transform_scenario('p300')",
+                           "transform_scenario('ssvep')",
+                           "",
+                           "",
+                           "transform_scenario('p300_calib')",
+                           "transform_scenario('ssvep_calib')",
+                           self._finish_action()]
+
+        self.actions_solver = self.number_of_states * [self.number_of_decisions * [""]]
+
+    def _finish_action(self):
+        return "finish()"
