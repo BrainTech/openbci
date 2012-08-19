dce5a2833cc7a03c988d65422c1617742bbab756
Karol Auguštin <karol@augustin.pl>
2012-08-20 00:08:35 +0200

    Install scripts reorganisation

---
+++ scripts/openbci_add_deb_repo_precise.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sudo wget -O /etc/apt/sources.list.d/deb.braintech.pl-ubuntu-precise.list http://deb.braintech.pl/deb.braintech.pl-ubuntu-precise.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1452AFDF
--- scripts/openbci_install.sh
+++ scripts/openbci_install.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 deps='git-core python-dev libboost-all-dev libbluetooth-dev xsel python-dev python-serial python-pygame python-scipy python-numpy python-sip python-qt4 python-bluetooth python-xlib libzmq-dev python-zmq python-pyaudio libprotobuf-dev'
 
-sudo wget -O /etc/apt/sources.list.d/deb.braintech.pl-ubuntu-precise.list http://deb.braintech.pl/deb.braintech.pl-ubuntu-precise.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1452AFDF
+./openbci_add_deb_repo_precise.sh
 sudo apt-get update
 sudo apt-get install $deps tmsi-dkms
 
