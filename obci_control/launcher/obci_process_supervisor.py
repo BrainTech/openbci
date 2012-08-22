603bdb880395630dfd452e1bbd03ee251b181ff7
Joanna Tustanowska <joanna.tustanowska@titanis.pl>
2012-08-22 11:49:39 +0200

    error in parsing mx address fix

--- obci_control/launcher/obci_process_supervisor.py
+++ obci_control/launcher/obci_process_supervisor.py
@@ -180,7 +180,7 @@ class OBCIProcessSupervisor(OBCIControlPeer):
         if 'mx' not in self.launch_data:
             mx_addr = message.mx_data[1].split(':')
             mx_addr[1] = int(mx_addr[1])
-            self.mx_data[0] = mx_addr
+            self.mx_data = tuple(mx_addr)
             self.env = self.peer_env(self.mx_data)
         self._launch_processes(self.launch_data)
 
