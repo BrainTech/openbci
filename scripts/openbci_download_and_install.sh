b84c2d27b42c4c27bc8d89cef9954bb364ae75e3
Karol Auguštin <karol@augustin.pl>
2012-08-23 17:15:27 +0200

    Fix git repository path in install scripts

--- scripts/openbci_download_and_install.sh
+++ scripts/openbci_download_and_install.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get install git
 
-git clone git://git.braintech.pl/openbci openbci stable
+git clone -b stable git://git.braintech.pl/openbci.git openbci
 cd openbci/scripts
 ./openbci_install.sh
