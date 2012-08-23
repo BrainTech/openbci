42471fd24671eb65d07afdacf19f2d5d6a36890d
Karol Auguštin <karol@augustin.pl>
2012-08-23 17:22:21 +0200

    Delete invocation of enviroment preparation script

--- scripts/openbci_install.sh
+++ scripts/openbci_install.sh
@@ -4,5 +4,3 @@ deps='git-core python-dev libboost-all-dev libbluetooth-dev xsel python-dev pyth
 ./openbci_add_deb_repo_precise.sh
 sudo apt-get update
 sudo apt-get install $deps tmsi-dkms openbci-libmultiplexer-dev openbci-dummy-amplifier openbci-file-amplifier openbci-libmultiplexer0 openbci-multiplexer openbci-multiplexer-python openbci-tmsi-amplifier
-
-sudo python obci_env_prepare.py `cd openbci && pwd` `whoami`
