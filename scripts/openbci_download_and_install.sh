4dc3d34cd109987011174a8983099dd3da6ac237
Karol Auguštin <karol@augustin.pl>
2012-08-23 17:11:57 +0200

    Fix new line in openbci_download_and_install script

--- scripts/openbci_download_and_install.sh
+++ scripts/openbci_download_and_install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh               
 
-sudo apt-get install git                                                       git clone git://git.braintech.pl/openbci openbci stable
+sudo apt-get install git
+
+git clone git://git.braintech.pl/openbci openbci stable
 cd openbci/scripts
 ./openbci_install.sh
