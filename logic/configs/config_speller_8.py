dfac166d6d612ef9a1a4c2e58b07f2a4d60ec317
Mateusz Kruszyński <mateusz.kruszynski@titanis.pl>
2012-09-05 15:38:02 +0200

    Muniich enhancements - part 37

--- logic/configs/config_speller_8.py
+++ logic/configs/config_speller_8.py
@@ -29,7 +29,7 @@ class Config(object):
 
        # Letters definition for every state. Normally for every state it should be a collection of strings.
         self.letters = self.number_of_states * [self.number_of_decisions * [""]]
-        self.letters[0] = [u"A B C\nD E F",u"G H I\nJ K L",u"M N O\nP R S",u"T U W\nX Y Z",u"Ą Ę Ł\nŃ Ó Ś", u"_,.;?!", u"AKCJE\nactions", u"WRÓĆ\n  back    "]
+        self.letters[0] = [u"A B C\nD E F",u"G H I\nJ K L",u"M N O\nP R S",u"T U W\nX Y Z",u"Ą Ę Ł\nŃ Ó Ś", u"_,.;?!", u" akcje\nactions", u"  wróć\n  back    "]
         self.letters[1] = ["A","B","C","D","E","F","  skasuj  \nbackspace", u"wróć\nback"]
         self.letters[2] = ["G","H","I","J","K","L","  skasuj  \nbackspace", u"wróć\nback"]
         self.letters[3] = ["M","N","O","P","R","S","  skasuj  \nbackspace", u"wróć\nback"]
