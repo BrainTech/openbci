1e56b7e5ca4f27f447c9c3015916e3581c213428
Anna Chabuda <anna.chabuda@gmail.com>
2013-11-07 17:56:41 +0100

    changes in wii board amplifier - necessary for Windows

--- drivers/balance/wii_board_amplifier_peer.py
+++ drivers/balance/wii_board_amplifier_peer.py
@@ -12,35 +12,45 @@ import pygame
 class WiiBoardAmplifier(ConfiguredClient):
     @log_crash
     def __init__(self, addresses):
-        super(WiiBoardAmplifier, self).__init__(addresses=addresses, 
+        super(WiiBoardAmplifier, self).__init__(addresses=addresses,
                                                 type=peers.AMPLIFIER)
-        operating_system = self.config.get_param('operating_system')
-        if operating_system == 'Linux':
+        self.operating_system = self.config.get_param('operating_system')
+        if self.operating_system == 'Linux':
             import wiiboard
-        elif operating_system == 'Windows':
-            import wiiboard_win as wiiboard
-        else:
-            self.logger.info("wrong operating system : operating_system=" + + "can be Linux ar Windows")
-            sys.exit(1)
-        self.logger.info("Start initializwiing wii board amplifier")
-        self.board = wiiboard.Wiiboard()
-        self.logger.info("Connecting to Wiiboard..")
-        self.logger.info("Please press the red 'connect' button on the balance board, inside the battery compartment.")
-        operating_system = self.config.get_param('operating_system')
 
-        try:
-            bord_address = self.board.discover()
-            self.logger.info("Found Wiiboard at address " + bord_address)
-        except Exception as e:
-            self.logger.error(e)
+            self.logger.info("Start initialization wii board amplifier")
+            self.board = wiiboard.Wiiboard()
+            self.logger.info("Connecting to Wiiboard..")
+            self.logger.info("Please press the red 'connect' button on the balance board, inside the battery compartment.")
+            try:
+                bord_address = self.board.discover()
+                self.logger.info("Found Wiiboard at address " + bord_address)
+            except Exception as e:
+                self.logger.error(e)
+                sys.exit(1)
+            try:
+                self.board.connect(bord_address)
+                self.logger.info("Connected to Wiiboard at address " + bord_address)
+
+            except Exception as e:
+                self.logger.error(e)
+                sys.exit(1)
+
+        elif self.operating_system == 'Windows':
+            import wiiboard_win as wiiboard
+
+            self.logger.info("Start initialization wii board amplifier")
+            self.board = wiiboard.Wiiboard()
+            try:
+                self.logger.info("Connecting to Wiiboard...")
+                self.board.connect()
+            except Exception as e:
+                self.logger.error(e)
+                sys.exit(1)
+        else:
+            self.logger.info("wrong operating system : operating_system=" + operating_system + " (can be Linux or Windows)")
             sys.exit(1)
-        try:
-            self.board.connect(bord_address)
-            self.logger.info("Connected to Wiiboard at address " + bord_address)
-        
-        except Exception as e:
-            self.logger.error(e)
-            sys.exit(1)
+        global wiiboard
         pygame.init()
         self.done = False
         self.ready()
@@ -54,9 +64,9 @@ class WiiBoardAmplifier(ConfiguredClient):
             x = 0 # ?
             y = 2 # ?
         else:
-            x = ((event.mass.topRight + event.mass.bottomRight) - 
+            x = ((event.mass.topRight + event.mass.bottomRight) -
                  (event.mass.topLeft + event.mass.bottomLeft))/event.mass.totalWeight
-            y = ((event.mass.topRight + event.mass.topLeft) - 
+            y = ((event.mass.topRight + event.mass.topLeft) -
                  (event.mass.bottomRight + event.mass.bottomLeft))/event.mass.totalWeight
         sample.channels.extend([x, y])     
         return packet
@@ -64,24 +74,25 @@ class WiiBoardAmplifier(ConfiguredClient):
     def disconnect(self):
         self.board.disconnect()
 
-    @log_crash     
+    @log_crash
     def run(self):
+        if self.operating_system == 'Windows': 
+			wiiboard.startThread()
         while (not self.done):
             for event in pygame.event.get():
                 if event.type == wiiboard.WIIBOARD_MASS:
                     msg = self.get_msg(event)
-                    self.conn.send_message(message = msg.SerializeToString(), 
+                    self.conn.send_message(message = msg.SerializeToString(),
                                            type = types.AMPLIFIER_SIGNAL_MESSAGE, flush=True)
-                    self.logger.info("Send wii board msg:"+str(msg.x)+" / "+str(msg.y))     
                 elif event.type == wiiboard.WIIBOARD_BUTTON_PRESS:
                     self.logger.info("Button pressed!")
                     #self.disconnect()
-                elif event.type == wiiboard.WIIBOARD_BUTTON_RELEASE: #niepotrzebne?
+                elif event.type == wiiboard.WIIBOARD_BUTTON_RELEASE:
                     self.logger.info("Button released")
                 elif event.type == wiiboard.WIIBOARD_DISCONNECTED:
                     self.logger.info("Wiiboard diconected")
                     self.done = True
         pygame.quit()
-                 
+
 if __name__ == "__main__":
     WiiBoardAmplifier(settings.MULTIPLEXER_ADDRESSES).run()
