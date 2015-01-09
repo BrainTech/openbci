3ad69af2f253f7a0fc7fbe14dc4af24af158183f
Anna Chabuda <anna.chabuda@gmail.com>
2015-01-09 12:36:40 +0100

    ventures exp logic can now finish signal saving

--- exps/ventures/logic_ventures_experiment.py
+++ exps/ventures/logic_ventures_experiment.py
@@ -4,12 +4,15 @@
 from multiplexer.multiplexer_constants import peers, types
 from obci.control.peer.configured_multiplexer_server import ConfiguredMultiplexerServer
 from obci.configs import settings, variables_pb2
+from obci.acquisition import acquisition_helper
 #from obci.gui.ugm import ugm_helper
 import maze
 import random, time, sys, thread
 
 class LogicVenturesExperiment(ConfiguredMultiplexerServer):
     def __init__(self, addresses, p_type = peers.LOGIC_WII_BOARD):#todo - change peer type
+        self._finish = False
+        self._addresses = addresses
         super(LogicVenturesExperiment, self).__init__(addresses=addresses, type=p_type)
         self._maze = maze.MazeGame('test')
         self.run_maze_in_thread()
@@ -19,6 +22,7 @@ class LogicVenturesExperiment(ConfiguredMultiplexerServer):
         self.logger.info("RUN MAZE START")
         self._maze.run()
         self.logger.info("RUN MAZE END")
+        self._finish = True
 
     def run_maze_in_thread(self):
         self.logger.info("RUN MAZE START THREAD ...")
@@ -34,7 +38,21 @@ class LogicVenturesExperiment(ConfiguredMultiplexerServer):
             self.logger.warning("Unrecognised message received! Ignore...")
         self.no_response()
 
+        if self._finish:
+            self.logger.info("start finish saving")
+            acquisition_helper.finish_saving(self._addresses, ['wii'])
+            self.logger.info("finish saving ended")
+            sys.exit(0)
+
 if __name__ == "__main__":
     LogicVenturesExperiment(settings.MULTIPLEXER_ADDRESSES).loop()
 
 ### todo: logic_wii_exp, analysis_wii_exp, maze mutex buffer, mx_rules_update, scenariusz, 
+
+### todo:
+# - jak bedzie odpalana sesna? jak definiowany user? jak scenariusze z rownowaga, sway, potem kalibracja?
+# - zrobic scenariusze powyzsze, ze sciezkami do odpowiednich plikow i przekazywaniem tego co trzeba
+# - modul w pythonie/pygame do rysowania kalibracji
+# - polaczenie scenariusza exp z wii boardem
+# - filtrowanie i przeplyw danycg driver - analiza - logika
+
