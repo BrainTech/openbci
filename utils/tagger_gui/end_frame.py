11eddd410e6b90f74f0981991c64669663b3e644
Anna Chabuda <anna.chabuda@gmail.com>
2015-01-15 20:30:34 +0100

    fix finish in tagger manula

--- utils/tagger_gui/end_frame.py
+++ utils/tagger_gui/end_frame.py
@@ -37,6 +37,6 @@ class EndFrame(QtGui.QFrame):
 
     def finish_action_elier_frame(self):
         self.elier_frame[0].finish_frame_action()
-        self.elier_frame.remove(self.next_frame[0])
+        self.elier_frame.remove(self.elier_frame[0])
         self.set_off()
         self.finish_signal.emit()
