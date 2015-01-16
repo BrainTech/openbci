e04d7123cff089a17eb1a1c873bc40b49925801b
Anna Chabuda <anna.chabuda@gmail.com>
2015-01-16 23:36:29 +0100

    ventures baseline calculation template

---
+++ exps/ventures/analysis_stable.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+
+def calculate(file_path, file_name):
+    #out = xa,ya,xb,yb,xc,yc
+    #in_file+'.obci.xml',
+    #in_file+'.obci.raw',
+    #in_file+'.obci.tag')
+    return 1.0, 0.0, 0.0, 1.1, 0.0, 0.0
--- exps/ventures/logic_ventures_stable.py
+++ exps/ventures/logic_ventures_stable.py
@@ -5,18 +5,28 @@ import sys, os.path, time
 from multiplexer.multiplexer_constants import peers, types
 from obci.control.peer.configured_multiplexer_server import ConfiguredMultiplexerServer
 from obci.configs import settings, variables_pb2
-
+from obci.exps.ventures import analysis_stable
+from obci.exps.ventures.data import data_manager
 from obci.acquisition import acquisition_helper
+
 
 class LogicVenturesStable(ConfiguredMultiplexerServer):
     def __init__(self, addresses):
         super(LogicVenturesStable, self).__init__(addresses=addresses,
                                                 type=peers.CLIENT)
         self.ready()
+        user_id = self.get_param('user_id')
+        file_name = self.get_param('file_name')
+        file_path = self.get_param('file_path')
         self.logger.info("start waiting for saving finished")
         acquisition_helper.wait_saving_finished(addresses, ['wii'])
-        self.logger.info("saving finished! Start analysis...")
-        time.sleep(3)
+
+        self.logger.info("saving finished! Start analysis for: "+user_id+" , to file_path: "+file_path+" and file_name: "+file_name)
+        xa,ya,xb,yb,xc,yc = analysis_stable.calculate(file_path, file_name)
+        self.logger.info('Baseline calculated: '+','.join([repr(xa), repr(ya), repr(xb), repr(yb), repr(xc), repr(yc)]))
+
+        data_manager.baseline_set(user_id, xa, ya, xb, yb, xc, yc, file_name)
+        self.logger.info('Baseline data properly stored!')
         sys.exit(0)
     
 if __name__ == "__main__":
