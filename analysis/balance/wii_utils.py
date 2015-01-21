7eddc2b49b7ac40b8e168c45efd3cd21b471ca41
Anna Chabuda <anna.chabuda@gmail.com>
2015-01-21 17:28:32 +0100

    change wii_utils: posible to use for matrix and scalars

--- analysis/balance/wii_utils.py
+++ analysis/balance/wii_utils.py
@@ -8,7 +8,7 @@ def get_x_y(tl, tr, br, bl):
     - br - bottom right
     - bl - bottom left
     in int or float format, return x, y in floats."""
-    sum_mass = float(tl+tr+br+bl)
+    sum_mass = tl+tr+br+bl
     x = (((tr + br) - (tl + bl))/sum_mass)
     y = (((tr + tl) - (br + bl))/sum_mass)
     return x, y
