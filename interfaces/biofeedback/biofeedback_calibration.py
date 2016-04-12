6f9cc3a6b5c29f586c39541c6f3e35179f2d33a2
Anna Chabuda <anna.chabuda@gmail.com>
2016-04-12 17:55:12 +0100

    add simple biofeedback calibration logic peer

---
+++ interfaces/biofeedback/biofeedback_calibration.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+
+def biofeedback_calibration_run(user_name, target_count):
+    print "running...."
+    time.sleep(10)
---
+++ interfaces/biofeedback/biofeedback_calibration_logic_peer.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author:
+#     Anna Chabuda <anna.chabuda@gmail.com>
+
+import time, sys
+
+from multiplexer.multiplexer_constants import peers, types
+from obci.control.peer.configured_multiplexer_server import ConfiguredMultiplexerServer
+
+from obci.configs import settings, variables_pb2
+from obci.acquisition import acquisition_helper
+from obci.interfaces.biofeedback import biofeedback_calibration
+
+class BiofeedbackCalibration(ConfiguredMultiplexerServer):
+    """A class for creating a manifest file with metadata."""
+    def __init__(self, addresses):
+        super( BiofeedbackCalibration, self).__init__(addresses=addresses,
+                                          type=peers.LOGIC_P300_CALIBRATION)
+        
+        self.target_count = int(self.config.get_param("target_count"))
+        self.user_name = self.config.get_param("user_name")
+
+        self.ready()
+        self.run()
+        
+    def run(self):
+        self.logger.info("START Biofeedback Calibration...")
+        biofeedback_calibration.biofeedback_calibration_run(self.user_name, self.target_count)
+        self.logger.info("FINISH Biofeedback Calibration...")
+
+        acquisition_helper.send_finish_saving(self.conn)
+
+        time.sleep(5)
+        sys.exit(0)
+
+if __name__ == "__main__":
+    BiofeedbackCalibration(settings.MULTIPLEXER_ADDRESSES).loop()
